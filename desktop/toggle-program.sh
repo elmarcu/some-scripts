11b0b38ba58a4d7d3664ab5c1e1eb48396d2a71a
Ale <elmarcu@gmail.com>
2021-04-08 16:04:14 -0300

    toggle-program | pause or continue program's execution

---
+++ desktop/toggle-program.sh
@@ -0,0 +1,34 @@
+#! /bin/sh
+#
+stopped=false
+found=false
+program=$1
+binary=$2
+if [ -z $binary ] ; then
+  binary="exe"
+fi
+
+for i in $(ps aux | grep $program | grep \.$binary | awk '{print $8}'); do
+  found=true
+  if [ "TNl" = "$i" ] || [ "Tsl" = "$i" ] || [ "Tl" = "$i" ] ; then
+    stopped=true
+  fi
+done
+
+if [ "$found" = true ] ; then
+
+  if [ "$stopped" = true ] ; then
+    sh $HOME/workspace/some-scripts/desktop/throttling-cpu.sh gamer
+  else
+    sh $HOME/workspace/some-scripts/desktop/throttling-cpu.sh
+  fi
+
+  if [ "$stopped" = true ] ; then
+    echo "Programs changing to running"
+    ps aux | grep $program | grep \.$binary | awk '{print $2}' | xargs kill -CONT
+  else
+    echo "Programs changing to pausing"
+    ps aux | grep $program | grep \.$binary | awk '{print $2}' | xargs kill -STOP
+  fi
+  ps aux | grep $program | grep \.$binary
+fi
