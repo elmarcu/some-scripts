19a458e2985a62ff585f9086862d8033f4fd0f61
Ale <elmarcu@gmail.com>
2025-03-12 10:47:46 -0400

    small fix

--- workspace-unix/custom-settings.sh
+++ workspace-unix/custom-settings.sh
@@ -101,7 +101,7 @@ echo 'export PATH="/opt/homebrew/bin/gh:$PATH"' >> $HOME/.zshrc
 
 #vpn
 brew install openconnect
-#sudo openconnect --protocol=gp yuor-company-vpn.com -u [USER] --passwd-on-stdin
+#sudo openconnect --protocol=gp your-company-vpn.com -u [USER] --passwd-on-stdin
 
 #executables
 mkdir -p $HOME/.bin
